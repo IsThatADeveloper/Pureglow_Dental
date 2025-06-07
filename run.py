bca6ed0b94e158977d44c0c670546175e3942c0d
unknown <eshaanshah24@gmail.com>
2025-06-06 20:29:49 -0400

    Fix host/port for Render deployment

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import os
 if __name__ == '__main__':
     # app.run(debug=True)
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='44.225.181.72', port=port)
     
 
 
